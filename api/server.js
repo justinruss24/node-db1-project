14c0de819e2043c4423f70765a6d7a7378556bd4
Justin Russell <Justin@Justins-MacBook.local>
2020-04-13 13:58:03 -0700

    initial commit

--- api/server.js
+++ api/server.js
@@ -6,4 +6,47 @@ const server = express();
 
 server.use(express.json());
 
+server.get("/accounts", (req, res) => {
+    db.select("*")
+    .from("accounts")
+    .then(accounts => {
+        res.status(200).json(accounts);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ message: "Error retrieving accounts" });
+    })
+});
+
+server.get("/accounts/:id", (req, res) => {
+    db("accounts")
+    .where({ id: req.params.id })
+    .first()
+    .then(account => {
+        if (account) {
+            res.status(200).json({ data: account })
+        } else {
+            res.status(404).json({ error: "id not found" })
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: error.message })
+    })
+});
+
+server.post("/accounts/:id", (req, res) => {
+    db("accounts")
+    .insert(req.body)
+    .then(account => {
+        res.status(201).json(account)
+    })
+    .catch(error => {
+        res.status(500).json({ error: error.message })
+    })
+})
+
+
+
+
 module.exports = server;
